95e21a662c6ac33180badf60acf8b5d0be4b96ca
Dingchao Zhang <dingchaozhang@g.harvard.edu>
2017-02-03 21:59:22 -0600

    added files to calcu pca and tsned sim coordiante

---
+++ cs231n/assignment1/cs231n/classifiers/simzip.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot a plt
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+
+zipdf = pd.read_csv('zipmerged2010-2015.csv')
+zipdf_num = zipdf.iloc[:,3:] # remove zip code, city name, fips code for now
+
+#There are 1428 columns for 2011 to 2015 demographics data
+# Use PCA to reduce to 50 before using TSNE to reduce computation efficiency
+pca = PCA(n_components= 50)
+pcaed = pca.fit_transform(zipdf_num)
+
+# Use TSNE to reduce to 2 dimnesions
+tsne = TSNE(n_components=2,random_state = 0)
+tsned = tsne.fit_transform(pcaed)
+
+# Save tsned np array into a dataframe
+tsnedf = pd.DataFrame(tsned,columns  = ['x','y'],index = None)
+# Append back zip code, city name, fips code
+zipsim_df= pd.concat((tsnedf,zipdf.iloc[:,:3]),axis = 1)
+# Save to a csv
+zipsim_df.to_csv('zipsimilarity_tsne.csv',index = None)
+
+# save tsned file to a csv, it takes long to run the tsne
+
+
+## TO-DO:
+# zipsim_df = pd.read_csv('/san-data/usecase/agentpm/AgentProductionModel/zipsimilarity_tsne.csv')
+#zipinfo = pd.read_csv('us_postal_codes.csv')
+# Create functio to return most similar zip
+# Aggregate by city to validate that ny == chicago more than ny == bloomington,il
+# sth like this to verify that if we cluster them using kmeans, we found large cities clutered 
+# in one place and we color them by category: surburban, urban, rural areas
+
+# Visualize
+# plt.scatter(zipsim_df.iloc[:,0],zipsim_df.iloc[:,1])
+# # D is the city + zip code name
+# for i in xrange(D):
+# 	plt.annotate(s = index[i],xy = (tsned[i,0],tsned[i,1]))
+# plt.show()
