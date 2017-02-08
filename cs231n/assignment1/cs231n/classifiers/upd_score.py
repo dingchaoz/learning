fc0fe4edf945c4291a59a11edfdb425ddd17091e
Dingchao Zhang <dingchaozhang@g.harvard.edu>
2017-02-08 12:39:04 -0600

    completed the update score and extract script

--- cs231n/assignment1/cs231n/classifiers/upd_score.py
+++ cs231n/assignment1/cs231n/classifiers/upd_score.py
@@ -1,53 +1,51 @@
 import glob
 import pandas as pd
 import numpy as np
-import os.path
+import os
 
 
 if __name__ == '__main__':
 
-
+	os.chdir('/san-data/atlas_id/')
 	novScore = pd.read_csv('nov_time_series_scores.csv')
 	novScore = novScore[['ASSOC_ID','score']]
 	novScore['ASSOC_ID'] =  novScore['ASSOC_ID'].astype(str)
 
 	csvFiles = glob.glob("ontrans/atlsOnTrans2017*csv")
+	agtOfficeFile = 'logs/agent_select_score_log_file.txt'
+	save_path = 'ontrans/'
+	agtOfficeDF = pd.read_csv(agtOfficeFile,sep = '|')
+	agtOfficeDF=agtOfficeDF.rename(columns = {'AGT_ASSOC_ID':'ASSOC_ID'})
+	novScoreMerged = novScore.merge(agtOfficeDF,on = 'ASSOC_ID',how = 'left')
+	TopMaxAgt = []
 
-    save_path = 'ontrans/'
+	for csv in csvFiles:
 
-    # Read csv files
-    for csv in csvFiles:
-        date = csv.split('ontrans/atlsOnTrans')[1][:8] # Get the date
-
-        df_on = pd.read_csv(csv)
-        df_on['ASSOC_ID'] =  df_on['ASSOC_ID'].astype(str)
-        # Need to map first with /san-data/atlas_id/logs/agent_select_score_log_file.txt
-        # agent associate id mapping with office associate id
-        #novScore[novScore.ASSOC_ID == '003BC472000']
-        #df_on[df_on.ASSOC_ID == 'MF3SD6XS3AL']
-        #df_on.ASSOC_ID.isin(novScore.ASSOC_ID)
-        #grep '003BC472000' listval_201701*.xml
-        # nothing returns
-        #but it returns in the nov score
-        #grep 'MF3SD6XS3AL' listval_20170101_235902.xml
-        # tihs returns in aol data
-        # but not return anything in the nov score file
-        # grep 'MF3SD6XS3AL' nov_time_series_scores.csv
-
-        # Doesn't work
-        #df_on.merge(novScore,on = 'ASSOC_ID',how = 'left')
-
-
-
-
-        df_MaxScoreAgent = df_on.sort('ATLS_SCORE',ascending = False).groupby('Trans',as_index = False).first()
+    	date = csv.split('ontrans/atlsOnTrans')[1][:8] # Get the date
+    	df_on_raw = pd.read_csv(csv)
+    	df_on_raw=df_on_raw.rename(columns = {'ASSOC_ID':'#OFFC_ASSOC_ID'})
+        # Works now
+        df_on = df_on_raw.merge(novScoreMerged,on = '#OFFC_ASSOC_ID',how = 'left')
+        del df_on['ATLS_SCORE']
+        del df_on['#OFFC_ASSOC_ID']
+        del df_on['ATLS_APPLIED']
+        del df_on['TERRITORY']
+        del df_on['SCORE']
+        del df_on['SHUFFLE_PARM']
+        del df_on['AVERAGE_SCORE']
+        del df_on['FINAL_SCORE']
+        del df_on['ACTN_CD']
+        
+        df_MaxScoreAgent = df_on.sort('score',ascending = False).groupby('Trans',as_index = False).first()
         df_TopAgent = df_on[df_on.Pos == 1]
         
-        df_TopAgent.columns = ['TopPos','TopScore','TopAgent','ATLS_APPLIED','Date','Trans']
+        #df_TopAgent.columns = ['TopPos','TopScore','TopAgent','ATLS_APPLIED','Date','Trans']
+        df_TopAgent=df_TopAgent.rename(columns = {'Pos':'TopPos','score':'TopScore','ASSOC_ID':'TopAgent'})
+
         del df_MaxScoreAgent['Date']
-        del df_MaxScoreAgent['ATLS_APPLIED']
-        df_MaxScoreAgent.columns = ['Trans','MaxScorePos','MaxScore','MaxAgent']
-        df_total = pd.merge(df_TopAgent,df_MaxScoreAgent,on = 'Trans')
+        df_MaxScoreAgent.columns = ['Trans','MaxScorePos','MaxAgent','MaxScore']
+        #df_total = pd.merge(df_TopAgent,df_MaxScoreAgent,on = 'Trans')
+        df_total = df_TopAgent.merge(df_MaxScoreAgent,on = 'Trans').sort('Trans')
 
         TopMaxAgt.append(df_total)
         print 'Extracted top and max agents on this date: ' +  date
@@ -58,7 +56,6 @@ if __name__ == '__main__':
     filename = 'TopMaxAgt201701.csv'
     compfilename = os.path.join(save_path,filename )
     TopMaxAgt_df.to_csv(compfilename,index = None)
-
     print 'Extractions on all files completed, saved as: ' + filename
 
 
