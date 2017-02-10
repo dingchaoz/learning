798bc566d882696924ff2993c1dd9ad628abea4a
Dingchao Zhang <dingchaozhang@g.harvard.edu>
2017-02-10 08:40:03 -0600

    start writing production script

---
+++ cs231n/assignment1/cs231n/classifiers/agtProduction.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import os
+# utm is installed by running python setup.py install in the directory at ejlq@da74wbedge1 [/home/ejlq/utm-0.4.1]
+import utm
+import matplotlib.pyplot as plt
+from scipy.spatial.distance import pdist, squareform
