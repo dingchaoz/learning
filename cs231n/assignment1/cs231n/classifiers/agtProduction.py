f6cfb5d122ffac64b09bf0a0db3abbfadba90ff4
Dingchao Zhang <dingchaozhang@g.harvard.edu>
2017-02-10 10:39:37 -0600

    start writing preproceessind data script

--- cs231n/assignment1/cs231n/classifiers/agtProduction.py
+++ cs231n/assignment1/cs231n/classifiers/agtProduction.py
@@ -5,3 +5,20 @@ import os
 import utm
 import matplotlib.pyplot as plt
 from scipy.spatial.distance import pdist, squareform
+
+os.chdir('/san-data/usecase/agentpm/AgentProductionModel')
+
+
+# Add headers and new col
+# Read sum aggregated auto prem columns grouped by state and agent code 
+agtSumDF = pd.read_csv('auto_sum_stagt.csv',header = None)
+# Add columns headers, the headers are in APM/Agent_Production_model.ipynb file
+agtSumDF.columns = headers
+# Create state agent column combining state and agent cols
+agtSumDF.insert(1,'STCODE',[str(x)+str(y) for x,y in zip(agtSumDF['STATE'],agtSumDF['AGENT'])])
+# Save to csv with added staget col and headers
+agtSumDF.to_csv('auto_sum_stagt.csv',index = False)
+# Drop NA any
+agtSumDF.dropna(how = 'any',axis = 1, inplace = True)
+# Save NA dropped file
+agtSumDF.to_csv('auto_sum_stagt_nadropped.csv',index = False)
