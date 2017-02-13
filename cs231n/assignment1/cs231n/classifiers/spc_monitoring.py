046a607ab7e248281d9fa14ea0dd624765b5dea2
Dingchao Zhang <dingchaozhang@g.harvard.edu>
2017-02-13 14:47:12 -0600

    finished monitoring script 1st ver

--- cs231n/assignment1/cs231n/classifiers/spc_monitoring.py
+++ cs231n/assignment1/cs231n/classifiers/spc_monitoring.py
@@ -6,8 +6,10 @@ import math
 import numpy as np
 import time
 import datetime
-from sklearn import metrics
 import matplotlib.pyplot as plt
+import json
+from sklearn import metrics
+from spc import *
 pd.options.display.max_columns = 100
 pd.options.display.max_rows = 500
 
@@ -29,6 +31,68 @@ atlas_nov = atlas[atlas.QUOT_MONTH_new == '2016-11-01'][['ASSOC_ID','CONV_RATE']
 nov_merged = nov.merge(atlas_nov,on ='ASSOC_ID')
 # Compute residual of forecast
 nov_merged['ERR'] = nov_merged.CONV_RATE - nov_merged.score
+# Update names of the columns
+
+# Get last 4 month data only preparing compute thresh
+last4M = atlas[atlas['QUOT_MONTH_new'] <= '2016-11-01']
+# Apply filter of 40 counts or not
+thresh = last4M.groupby('ASSOC_ID')['QUOT_CNT'].agg(np.sum) >= 40
+# Reset index
+thresh = thresh.reset_index()
+# Filter out not meeting quote counts agent
+thresh = thresh[thresh.QUOT_CNT == True]
+# Delete true or false col
+del thresh['QUOT_CNT']
+
+
+# Merge into the final data frame
+df_final = df_final.merge(nov_merged, on= 'ASSOC_ID')
+# Merge with threshed agents
+df_final = df_final.merge(thresh, on= 'ASSOC_ID', how = 'inner')
+
+
+
+## SPC running
+ERRS = df_final.filter(regex = 'ERR_')
+dlist = []
+
+for i in range(0,ERRS.shape[0]):
+    x = ERRS.iloc[i,:].tolist()
+    cc = Spc(x, CHART_X_MR_X)
+    if len(cc._find_violating_points()) > 0:
+        d = {}
+        d[df_final.iloc[i,0]] = cc._find_violating_points()
+        dlist.append(d)
+#     cc.get_chart()
+#     plt.title(df_final2.iloc[i,0] + ' forecast error control chart')
+#     plt.annotate(cc._find_violating_points(), xy=(0.05, 0.85), xycoords='axes fraction')
+#     plt.xticks(range(0,10),labels,rotation = 'horizontal')
+#     pp.savefig(fig)
+#     plt.close(fig)
+    
+#pp.close()
+#print dlist
+
+
+with open('/san-data/usecase/atlasid/new_data/output_file/spcRun', 'w') as fout:
+    json.dump(dlist, fout)
+
+outlierInfo = []
+with open('/san-data/usecase/atlasid/new_data/output_file/spcRun') as f:
+    for line in f:
+        outlierInfo.append(json.loads(line))
+
+
+outlier2_3M = {}
+for i in outlierInfo[0]:
+    if (len(i.values()[0].values()) >=2):
+        outlier2_3M[i.keys()[0]] = (i.values()[0].values())
+ 
+outlier2cont = {}       
+for key, value in outlier2_3M.iteritems():
+	if (value[1][0] - value[0][-1] == 1) and (value[1][0] == 10) :
+		outlier2cont[key] = value
+
 
 
 # jan = pd.read_csv('/san-data/usecase/atlasid/new_data/output_file/jan_time_series_scores.csv')
