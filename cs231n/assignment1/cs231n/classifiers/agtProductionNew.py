8f64b5ae5361ec841ab6df3b93316312e8e594f4
Dingchao Zhang <dingchaozhang@g.harvard.edu>
2017-02-24 21:28:58 -0600

    added notes about data pull

--- cs231n/assignment1/cs231n/classifiers/agtProductionNew.py
+++ cs231n/assignment1/cs231n/classifiers/agtProductionNew.py
@@ -1,1 +1,15 @@
-import pyspark
+#import pyspark
+from pyspark import SparkContext, HiveContext, SparkConf
+import os
+import pandas as pd
+import numpy as np
+import cPickle as pickle
+import collections
+
+os.chdir('/san-data/usecase/agentpm/AgentProductionModel/')
+
+
+## Re query the agents' pif and prem data without aggregating other columns
+# may avoid the huge data file(>50 gs) and thus no need to spark
+# sc = pyspark.SparkContext(appName="myAppName")
+# sc._conf.getAll()
