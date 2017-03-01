309b8661d7d70a13dee64488ef894e39bbb6bfc1
Dingchao Zhang <dingchaozhang@g.harvard.edu>
2017-03-01 07:56:12 -0600

    added lines for dates compare

--- cs231n/assignment1/cs231n/classifiers/agtProductionNew.py
+++ cs231n/assignment1/cs231n/classifiers/agtProductionNew.py
@@ -13,3 +13,19 @@ os.chdir('/san-data/usecase/agentpm/AgentProductionModel/')
 # may avoid the huge data file(>50 gs) and thus no need to spark
 # sc = pyspark.SparkContext(appName="myAppName")
 # sc._conf.getAll()
+
+## Read the targets file
+targets = pd.read_csv('datapull/auto_agts_pif_prem_sum.csv',header = None)
+
+##the intial file had two additional columns and no headers, and they got corrected:
+# targets = targets.iloc[:,:7]
+# header = ['year','agent_cd','st_cd','pol_effective_date','pol_expiration_date','pifsum','premsum']
+# targets.columns = header
+# targets.to_csv('datapull/auto_agts_pif_prem_sum.csv',index = None)
+
+# Columns of effective and expirations dates can be compared directly
+targets.pol_effective_date[0] >  targets.pol_expiration_date[0]
+targets.pol_effective_date[0] >  '2011-01-01'
+
+##Use 2014 features predict for 2015
+features2014 = pd.read_csv('top10ZipFeatures/top10ZipFeatures_2014.csv')
