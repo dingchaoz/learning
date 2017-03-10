526b72e18bd6fa8c99a495122ed00d10aa5daec2
Dingchao Zhang <dingchaozhang@g.harvard.edu>
2017-03-10 17:32:35 -0600

    added mv notes, updated data pull notes

--- cs231n/assignment1/cs231n/classifiers/agtProductionModel.py
+++ cs231n/assignment1/cs231n/classifiers/agtProductionModel.py
@@ -82,3 +82,28 @@ pd.concat([predict,truth],axis = 1)
 
 ##the mae is 464, and the pifsum avg is about 1481, error too high
 
+###### For all agents model build: just try use last 3 years average
+###### load all years' targets
+targets2015 = pd.read_csv('datapull/targets2015.csv')
+targets2014 = pd.read_csv('datapull/targets2014.csv')
+targets2013 = pd.read_csv('datapull/targets2013.csv')
+targets2012 = pd.read_csv('datapull/targets2012.csv')
+targets2011 = pd.read_csv('datapull/targets2011.csv')
+
+targets2011.columns = ['agtstcode','pifsum2011','premsum2011']
+targets2012.columns = ['agtstcode','pifsum2012','premsum2012']
+targets2013.columns = ['agtstcode','pifsum2013','premsum2013']
+targets2014.columns = ['agtstcode','pifsum2014','premsum2014']
+targets2015.columns = ['agtstcode','pifsum2015','premsum2015']
+
+
+dfs = [targets2015,targets2014,targets2013,targets2012,targets2011]
+df_final = reduce(lambda left,right:pd.merge(left,right, on = 'agtstcode'),dfs)
+
+#take 3 year average from 2012 -2014
+df_final['3yrAvg'] = df_final[['pifsum2014','pifsum2013','pifsum2012']].mean(axis = 1)
+# the mae is 151, much smaller compared to 450 range in machine learning model
+mean_absolute_error(df_final['3yrAvg'], df_final.pifsum2015)
+
+# Now look at new agents only, see if 3 year would make sense
+
